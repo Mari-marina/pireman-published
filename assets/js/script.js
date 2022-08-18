5658e0b454c689a1741677e49d3616da21b33407
Mari-marina <83422160+Mari-marina@users.noreply.github.com>
2022-08-19 01:07:39 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ function test(a, b) {
     return a + b
 }
 
-$('.certificates__slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
+// $('.certificates__slider').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+// });
+
+
 //# sourceMappingURL=script.js.map
