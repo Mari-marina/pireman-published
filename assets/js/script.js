c2fc24ee61af420fec501c212d334a7e1f80d638
Mari-marina <83422160+Mari-marina@users.noreply.github.com>
2022-08-22 03:23:00 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,59 @@ function test(a, b) {
 }
 
 
+
+let openModalBtn = document.getElementById("modalOpen1");
+// let openModalBtn1 = document.getElementById("modalOpen1");
+let modal = document.querySelector(".modal");
+let closeModalBtn = document.querySelector(".close-btn");
+
+openModalBtn.addEventListener("click", function () {
+  modal.style.display = "flex";
+});
+// openModalBtn1.addEventListener("click", function () {
+//   modal.style.display = "flex";
+// });
+
+closeModalBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+let openModalBtn2 = document.getElementById("modalOpen2");
+let modalNovorossiysk = document.querySelector(".modal-novorossiysk");
+let closeModalBtn2 = document.querySelector(".close-btn2");
+
+openModalBtn2.addEventListener("click", function () {
+  modalNovorossiysk.style.display = "flex";
+});
+
+closeModalBtn2.addEventListener("click", function () {
+  modalNovorossiysk.style.display = "none";
+});
+window.addEventListener("click", function (event) {
+  if (event.target === modalNovorossiysk) {
+    modalNovorossiysk.style.display = "none";
+  }
+});
+
+let openModalBtn3 = document.getElementById("modalOpen3");
+let modalRostov = document.querySelector(".modal-rostov");
+let closeModalBtn3 = document.querySelector(".close-btn3");
+
+openModalBtn3.addEventListener("click", function () {
+  modalRostov.style.display = "flex";
+});
+
+closeModalBtn3.addEventListener("click", function () {
+  modalRostov.style.display = "none";
+});
+window.addEventListener("click", function (event) {
+  if (event.target === modalRostov) {
+    modalRostov.style.display = "none";
+  }
+});
 //# sourceMappingURL=script.js.map
